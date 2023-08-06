640d2a6c93e4f2613f7a9cfb6726a343a1d0c382
Rakibul Islam <103498045+Rakib7425@users.noreply.github.com>
2023-08-06 20:54:23 +0530

    added.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+import allReducers from './reducers/index';
 
+const store = createStore(
+  allReducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  );
 
-// ReactDOM.render(
-//   <App />,
-//   document.getElementById('root')
-// );
-
+ReactDOM.render(
+  <Provider store={store} ><App /></Provider>,
+  document.getElementById('root')
+);
